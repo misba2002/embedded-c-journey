82e62eedffa7242a50c18cdfda4d896002adaf63
misba2002 <misbajunneidi@gmail.com>
2026-07-09 11:11:32 +0530

    A prg to find middle number using logical operator

---
+++ middleL.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+int main()
+{
+    int n1, n2, n3;
+
+    printf("Enter the numbers: ");
+    scanf("%d%d%d", &n1, &n2, &n3);
+
+    if(n1>n2 && n1>n3)
+    {
+        if(n2>n3)
+        {
+            printf("%d is the middle number\n", n2);
+        }
+        else
+        {
+             printf("%d is the middle number", n3);
+        }
+    }
+    else if(n2>n1 && n2>n3)
+    {
+        if(n1>n3)
+        {
+             printf("%d is the middle number", n1);
+        }
+        else
+        {
+             printf("%d is the middle number", n3);
+        }
+    }
+    else
+    {
+        if(n1>n2)
+        {
+             printf("%d is the middle number", n1);
+        }
+        else
+        {
+              printf("%d is the middle number", n2);
+        }
+    }
+    return 0;
+}
