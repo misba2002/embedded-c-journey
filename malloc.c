6d289796d383e2a42eeb9286ef83f523a7a04f12
misba2002 <misbajunneidi@gmail.com>
2026-09-24 06:43:57 +0530

    an asic prg of malloc

---
+++ malloc.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int *ptr = malloc(20);
+    if(ptr==NULL)
+    {
+        return 0;
+    }
+    for(int i=0; i<5; i++)
+    {
+        scanf("%d", ptr +i );
+    }
+    printf("After storing\n");
+    for(int i=0; i<5; i++)
+    {
+        printf("%d ", ptr[i]);
+    }
+
+    free(ptr);
+}
